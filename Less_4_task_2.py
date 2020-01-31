0d84d44296994d451e7fe457224ca6a527711b62
igorprog <igorprog@yandex.ru>
2020-01-31 18:50:49 +0300

    Чутка подправил

--- Less_4_task_2.py
+++ Less_4_task_2.py
@@ -10,35 +10,35 @@
 
 # Первый — с помощью алгоритма «Решето Эратосфена».
 # на мой взгляд сложновато и запутанно получилось
-
-number = int(input('Введите порядковый номер искомого простого числа: '))
-STEP = 100  # Шаг поиска
-sieve = [i for i in range(STEP)]  # Первоначальный массив
-sieve[1] = 0
-res = []  # Результирующий массив простых чисел
-
-
-def simple_digit(number):
-    global res, sieve
-    if len(res) > number:
-        return res[number - 1]  # -1 чтобы убрать 0 из подсчета
-
-    for i in range(2, len(sieve)):
-        if sieve[i] != 0:
-            j = i + i
-            while j < len(sieve):
-                sieve[j] = 0
-                j += i
-
-    res = [i for i in sieve if i != 0]
-    sieve += [i for i in range(len(sieve), len(sieve) + STEP)]
-
-    return simple_digit(number)
-
-
-simple = simple_digit(number)
-print(res)
-print(f'Простое число с порядковым номером {number} = {simple}')
+#
+# number = int(input('Введите порядковый номер искомого простого числа: '))
+# STEP = 100  # Шаг поиска
+# sieve = [i for i in range(STEP)]  # Первоначальный массив
+# sieve[1] = 0
+# res = []  # Результирующий массив простых чисел
+#
+#
+# def simple_digit(number):
+#     global res, sieve
+#     if len(res) > number:
+#         return res[number - 1]  # -1 чтобы убрать 0 из подсчета
+#
+#     for i in range(2, len(sieve)):
+#         if sieve[i] != 0:
+#             j = i + i
+#             while j < len(sieve):
+#                 sieve[j] = 0
+#                 j += i
+#
+#     res = [i for i in sieve if i != 0]
+#     sieve += [i for i in range(len(sieve), len(sieve) + STEP)]
+#
+#     return simple_digit(number)
+#
+#
+# simple = simple_digit(number)
+# print(res)
+# print(f'Простое число с порядковым номером {number} = {simple}')
 
 # Второй — без использования «Решета Эратосфена».
 
@@ -53,7 +53,7 @@ def simple_digit(number):
     if len(res) > number:
         return res[number - 1]  # -1 чтобы убрать 0 из подсчета
 
-    # res = []
+    res = []
     for i in sieve:
         for j in range(2, i):
             if i % j == 0:
